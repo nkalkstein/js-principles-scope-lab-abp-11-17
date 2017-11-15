c70fd1286f77dd0a7b7dd56ec8c76bcda8b44d34
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-15 05:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var customerName = "bob";
 
 function upperCaseCustomerName() {
-  return customerName.toUpperCase;
+  const bigName = customerName.toUpperCase;
+  return bigName;
 }
 
 function setBestCustomer() {
